20fccb8d3e5e437af20896fc09e5316f170b06c1
ejj001 <chertangoldsmith@gmail.com>
2018-10-23 13:49:16 -0400

    Class

--- Programming II/Oct 24/src/SearchingArray.java
+++ Programming II/Oct 24/src/SearchingArray.java
@@ -7,22 +7,24 @@ public class SearchingArray {
     public static boolean search(int[] array, int num){
         int index = array.length/2;
 
-        if(array[index] < num){
-            return search(Arrays.copyOfRange(array, 0, index), num);
+        if(array[index] < num && num <= array[array.length - 1]){
+            return search(Arrays.copyOfRange(array, index + 1, array.length - 1), num);
         }
-        if(array[index] > num){
-            return search(Arrays.copyOfRange(array, index, array.length), num);
+        if(array[index] > num && num >= array[0]){
+            return search(Arrays.copyOfRange(array, 0, index - 1), num);
         }
 
         return(array[index] == num);
     }
 
+    private boolean search(int[] array, int num, int low, int high){
+
+    }
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
         int term = input.nextInt();
 
         System.out.println(search(primes, term));
-
     }
 }
