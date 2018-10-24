bfd317521611cbbda1abc988b5432a1439955423
Eli Jones <chertangoldsmith@gmail.com>
2018-10-24 19:58:00 -0400

    Binary Search working

--- Programming II/Oct 24/src/SearchingArray.java
+++ Programming II/Oct 24/src/SearchingArray.java
@@ -4,27 +4,34 @@ import java.util.Scanner;
 public class SearchingArray {
     private static int[] primes = new int[] {2,3,5,7,11,13,17,19,23,29};
 
-    public static boolean search(int[] array, int num){
-        int index = array.length/2;
-
-        if(array[index] < num && num <= array[array.length - 1]){
-            return search(Arrays.copyOfRange(array, index + 1, array.length - 1), num);
-        }
-        if(array[index] > num && num >= array[0]){
-            return search(Arrays.copyOfRange(array, 0, index - 1), num);
-        }
-
-        return(array[index] == num);
+    public static int search(int[] array, int num){
+        if(num > array[array.length-1] || num < array[0])
+            return -1;
+        return (search(array, num, 0, array.length-1));
     }
 
-    private boolean search(int[] array, int num, int low, int high){
+    private static int search(int[] array, int num, int low, int high){
+        int index = (high-low) + low;
 
+        if(index < 0)
+            return -1;
+
+        if(array[index] < num) {
+            //Num is above index, so index + 1 becomes new low
+            return search(array, num, index + 1, high);
+        }
+        if(array[index] > num) {
+            //Num is below index, thus index - 1 becomes new high
+            return search(array, num, low, index - 1);
+        }
+        return index;
     }
+
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
         int term = input.nextInt();
 
-        System.out.println(search(primes, term));
+        System.out.printf("%d found at %d", term, search(primes, term));
     }
 }
