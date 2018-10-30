57b3a7353f7f461f3cb4eaf0b1f4a3909831ea80
Eli Jones <chertangoldsmith@gmail.com>
2018-10-30 01:56:10 -0400

    Oct 30

---
+++ Programming II/Oct 30/src/CycleGenerator.java
@@ -0,0 +1,46 @@
+public class CycleGenerator extends AbstractGenerator {
+    int start, end;
+
+    public CycleGenerator(int start, int end){
+        this.start = start;
+        this.end = end;
+        reset();
+    }
+
+    public void reset(){
+        set(start);
+    }
+
+    public int next(){
+        int next = get() + 1;
+        if (next > end){
+            reset();
+            next = get();
+        }
+
+        set(next);
+        return next;
+    }
+
+    public static void main(String[] args){
+        CycleGenerator generator = new CycleGenerator(10,12);
+
+        System.out.println(generator.get());
+        generator.next();
+        System.out.println(generator.get());
+        generator.next();
+        System.out.println(generator.get());
+        generator.next();
+        System.out.println(generator.get());
+
+        generator.reset();
+
+        System.out.println(generator.get());
+        generator.next();
+        System.out.println(generator.get());
+        generator.next();
+        System.out.println(generator.get());
+        generator.next();
+        System.out.println(generator.get());
+    }
+}
---
+++ Programming II/Oct 30/src/RandomGenerator.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+
+public class RandomGenerator extends AbstractGenerator{
+
+    private int seed;
+    Random random = new Random();
+
+    public RandomGenerator(){
+        seed = (int)(Math.random() * 10000);
+        reset();
+    }
+
+    public void reset(){
+        random.setSeed(seed);
+        set(seed);
+    }
+
+    public int next(){
+        int next = random.nextInt();
+        set(next);
+        return next;
+    }
+
+    public static void main(String[] args){
+        RandomGenerator generator = new RandomGenerator();
+
+        System.out.println(generator.get());
+        generator.next();
+        System.out.println(generator.get());
+        generator.next();
+        System.out.println(generator.get());
+        generator.next();
+        System.out.println(generator.get());
+
+        generator.reset();
+
+        System.out.println(generator.get());
+        generator.next();
+        System.out.println(generator.get());
+        generator.next();
+        System.out.println(generator.get());
+        generator.next();
+        System.out.println(generator.get());
+    }
+}
