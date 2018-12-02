ad0212d0ded15e1e1e8f8317a0d3a4c2dd5b535d
Eli Jones <chertangoldsmith@gmail.com>
2018-12-01 23:57:45 -0500

    Assignment 6

---
+++ Python Lab/Assignment 6/test.py
@@ -0,0 +1,10 @@
+import analyze
+
+a = (1, 2, 5, 4, 8, 9, 7, 6, 3)
+
+print(analyze.minimum(a))
+print(analyze.maximum(a))
+print(analyze.k_largest(a, 3))
+print(analyze.median(a))
+print(analyze.average(a))
+print(analyze.standard_deviation(a))
