9a39641057bebd68e4d31d2b4d046429f9389b0c
Eli <chertangoldsmith@gmail.com>
2018-12-05 18:43:28 -0500

    Assignment 9

--- Python Lab/Assignment 9/Star_wars.py
+++ Python Lab/Assignment 9/Star_wars.py
@@ -7,34 +7,44 @@ def get_external(a):
     a = a.split("/")
     return a[5]
 
+def get(type, value):
+    if(type == "film"):
+        return swapi.get_film(value).title
+    if(type == "species"):
+        return swapi.get_species(value).name
+    if(type == "starships"):
+        return swapi.get_starship(value).name
+    if(type == "vehicles"):
+        return swapi.get_vehicle(value).name
 
-search = "Boba fet"
+
+search = "luke skywalker"
 
 url = 'https://swapi.co/api/people/'
 r = requests.get(url, params={'search': search})
 data = json.loads(r.text)
+print(data)
+data = data["results"][0]
 
 
 attributes = ("name", "birth_year", "eye_color", "gender", "hair_color", "height", "mass", "skin_color")
 for x in attributes:
-    print(x + ": " + data["results"][0][x])
+    print(x + ": " + data[x])
 
 #homeworld
-print("Homeworld: " + (swapi.get_planet(get_external(data["results"][0]["homeworld"]))).name)
+print("Homeworld: " + (swapi.get_planet(get_external(data["homeworld"]))).name)
 
-#films
-print("Films:")
-a = len(data["results"][0]["films"])
-for j in range(0, a):
-    print("    " + swapi.get_film(get_external(data["results"][0]["films"][j])).title)
+# #films
+# print("Films:")
+# a = len(data["films"])
+# for j in range(0, a):
+#     print("    " + swapi.get_film(get_external(data["films"][j])).title)
 
 
-attributes = ("species", "starships", "vehicles")
+attributes = ("films", "species", "starships", "vehicles")
 for x in attributes:
     print(x + ":")
-    a = len(data["results"][0][x])
-    for j in range(0, a):
+    for j in range(0, len(data[x])):
         # MOVE get_film to url search
-        print("    " + swapi.get_film(get_external(data["results"][0][x][j])).name)
-
-
+        #print("    " + swapi.get_film(get_external(data[x][j])).name)
+        print("    " + get(x, get_external(data[x][j])))
