e186d0736efca8cd90008e7d796c97ce2f128f19
Eli <chertangoldsmith@gmail.com>
2019-04-23 12:58:04 -0400

    Assignment 4

---
+++ C# Lab/Assignment 4/Fraction.cs
@@ -0,0 +1,31 @@
+public class Fraction{
+    private int numerator, denominator;
+
+    public Fraction(int num, int dem){
+        numerator = num;
+        denominator = dem;
+    }
+
+    private int getNum(){
+        return numerator;
+    }
+    private int getDem(){
+        return denominator;
+    }
+
+    public Fraction add(Fraction other){
+
+    }
+
+    public Fraction subtract(Fraction other){
+
+    }
+
+    public Fraction multiply(Fraction other){
+
+    }
+
+    public Fraction divide(Fraction other){
+        
+    }
+}
---
+++ C# Lab/Assignment 4/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Assignment_4{
+    class Program{
+        static void Main(string[] args){
+            
+        }
+    }
+}
