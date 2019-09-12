b4a0572dd85330bb449551ea3a423928f0cac105
Eli Jones <chertangoldsmith@gmail.com>
2019-09-12 01:53:03 -0400

    Project 2

---
+++ C++ Lab/Project 2/cmake-build-debug/CMakeFiles/Project_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_2.dir/main.cpp.o"
+  "Project_2.pdb"
+  "Project_2.exe"
+  "libProject_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
