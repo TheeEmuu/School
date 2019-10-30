9a1adb84bbe8cc95dc52952402a28d49931c2061
Eli Jones <chertangoldsmith@gmail.com>
2019-10-30 18:56:30 -0400

    Initilize and ready JAX-RS Project

--- Distributed Systems/Hearthstone API/src/main/java/org/emuu/hearthstone/api/DecksService.java
+++ Distributed Systems/Hearthstone API/src/main/java/org/emuu/hearthstone/api/DecksService.java
@@ -1,4 +1,38 @@
 package org.emuu.hearthstone.api;
 
-public class DecksService {
+public class DecksService implements Decks {
+    @Override
+    public String getDecks() {
+        return null;
+    }
+
+    @Override
+    public String getDecksFiltered(String filter) {
+        return null;
+    }
+
+    @Override
+    public String getArchetype(String archetype) {
+        return null;
+    }
+
+    @Override
+    public String getDeck(String archetype, String id) {
+        return null;
+    }
+
+    @Override
+    public String updateDeck(String archetype, String id) {
+        return null;
+    }
+
+    @Override
+    public String newDeck(String archetype, String id) {
+        return null;
+    }
+
+    @Override
+    public String deleteDeck(String archetype, String id) {
+        return null;
+    }
 }
