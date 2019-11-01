5df12ae8149790e699a71a781415ebeedfdba3c4
Eli Jones <chertangoldsmith@gmail.com>
2019-11-01 09:41:36 -0400

    Prims

--- Computational Problem Solving/Special Subtree/src/Main.java
+++ Computational Problem Solving/Special Subtree/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 // https://www.hackerrank.com/challenges/primsmstsub/problem
@@ -20,13 +21,43 @@ public class Main {
 
     }
 
-    static class Edge{
+    public static Vertex[] prims(Vertex[] graph){
+        Vertex[] mst = new Vertex[graph.length];
+        for(int i = 0; i < graph.length; i++){
+            graph[i] = new Vertex();
+        }
+
+        boolean[] visited = new boolean[graph.length];
+
+        Vertex first = graph[0];
+        visited[0] = true;
+        PriorityQueue<Vertex> queue = new PriorityQueue<>();
+        queue.add(first);
+
+        while(!queue.isEmpty()){
+            Vertex cur = queue.remove();
+            Edge bestWeight = new Edge(1, 100001);
+            for(Edge e : cur.edges){
+                if(e.compareTo(bestWeight) < 0)
+                    bestWeight = e;
+            }
+
+            mst[]
+        }
+    }
+
+    static class Edge implements Comparable<Edge> {
         int weight;
         int dest;
 
         Edge(int d, int w){
             dest = d;
             weight = w;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.weight - o.weight;
         }
     }
 
