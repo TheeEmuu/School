398bc6565c3d8c041c46dc2f37604a2db4fe7461
Eli Jones <chertangoldsmith@gmail.com>
2019-11-04 01:36:28 -0500

    Prims

--- Computational Problem Solving/Special Subtree/src/Main.java
+++ Computational Problem Solving/Special Subtree/src/Main.java
@@ -15,42 +15,62 @@ public class Main {
             graph[i] = new Vertex();
         }
         for(int i = 0; i < edges; i++){
-            graph[in.nextInt()].edges.add(new Edge(in.nextInt(), in.nextInt()));
+            int source = in.nextInt() - 1;
+            int dest = in.nextInt() - 1;
+            int weight = in.nextInt();
+            graph[source].edges.add(new Edge(source, dest, weight));
+            graph[dest].edges.add(new Edge(dest, source, weight));
         }
 
+        int startNode = in.nextInt() - 1;
 
+        Vertex[] mst = prims(graph, startNode);
+
+        int totalWeight = 0;
+        for(Vertex v : mst){
+            if(v.edges != null) {
+                for (Edge e : v.edges) {
+                    totalWeight += e.weight;
+                }
+            }
+        }
+
+        System.out.println(totalWeight);
     }
 
-    public static Vertex[] prims(Vertex[] graph){
+    public static Vertex[] prims(Vertex[] graph, int startNode){
         Vertex[] mst = new Vertex[graph.length];
         for(int i = 0; i < graph.length; i++){
-            graph[i] = new Vertex();
+            mst[i] = new Vertex();
         }
 
         boolean[] visited = new boolean[graph.length];
 
-        Vertex first = graph[0];
-        visited[0] = true;
-        PriorityQueue<Vertex> queue = new PriorityQueue<>();
-        queue.add(first);
+        Vertex first = graph[startNode];
+        visited[startNode] = true;
+        PriorityQueue<Edge> queue = new PriorityQueue<>(first.edges);
+
 
         while(!queue.isEmpty()){
-            Vertex cur = queue.remove();
-            Edge bestWeight = new Edge(1, 100001);
-            for(Edge e : cur.edges){
-                if(e.compareTo(bestWeight) < 0)
-                    bestWeight = e;
-            }
+            Edge cur = queue.remove();
 
-            mst[]
+            if(!visited[cur.dest]){
+                mst[cur.source].edges.add(cur);
+                visited[cur.dest] = true;
+                queue.addAll(graph[cur.dest].edges);
+            }
         }
+
+        return mst;
     }
 
     static class Edge implements Comparable<Edge> {
+        int source;
         int weight;
         int dest;
 
-        Edge(int d, int w){
+        Edge(int s, int d, int w){
+            source = s;
             dest = d;
             weight = w;
         }
